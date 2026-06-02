9d843211dad1025ebdc3434e6a209cb32409e481
Code-With-Khushiii <khushipal20706@gmail.com>
2026-06-02 23:33:56 +0530

    Update LinkedIn link in App component

--- src/App.tsx
+++ src/App.tsx
@@ -641,7 +641,7 @@ function App() {
             <div className="flex items-center">
               <Linkedin className="w-6 h-6 text-brown mr-6" />
               <a
-                href="#"
+                href="https://www.linkedin.com/in/janvi-kl-94a147253/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-white/80 font-light hover:text-white transition-colors"
